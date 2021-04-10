5bcda86576a70e651042bff92981fe4ace3549a3
Andre Betz <git@andrebetz.de>
2021-04-10 08:23:43 +0200

    some changes to path

--- software/server/iSpindelServerDateWriter.py
+++ software/server/iSpindelServerDateWriter.py
@@ -8,14 +8,13 @@ import time
 import os
 import sys
 
-filename = "iSpindel.csv"
-srv_port = 9501
-srv_host = "0.0.0.0"
+#-------------variablen------------------
+filepath   = "/home/pi/Diskstation/Andre/projekte/"
+srv_port   = 9501
+delim      = ","
+nline      = "\r\n"
 rcv_buffer = 256
-rcv_ack = chr(6)
-rcv_nak = chr(21)
-delim = ";"
-nline = "\r\n"
+#-------------variablen------------------
  
 def recv_handler(clientsock, addr) :
   inpstr = ""
@@ -28,65 +27,52 @@ def recv_handler(clientsock, addr) :
   user_token = ""
   interval = 0
   rssi = 0
-
+  inpstr = ""
+  
   while 1:
     data = clientsock.recv(rcv_buffer)
-    if "close" == data.rstrip():
-      clientsock.send(rcv_ack)
-      print(repr(addr) + " ACK sent. Closing.")
-      break
-    try:
-      inpstr += str(data.rstrip())
-      if inpstr[0] != "{":
-        clientsock.send(rcv_nak)
-        dbgprint(repr(addr) + " Not JSON.")
-        break
-      print(repr(addr) + " Input Str is now:" + inpstr)
-      if inpstr.find("}") != -1:
-        jinput = json.loads(inpstr)
-        spindle_name = jinput["name"]
-        spindle_id   = jinput["ID"]
-        angle        = jinput["angle"]
-        temperature  = jinput["temperature"]
-        battery      = jinput["battery"]
-        try:
-          gravity   = jinput["gravity"]
-          interval  = jinput["interval"]
-          rssi      = jinput["RSSI"]
-        except:
-          print ("Consider updating your iSpindel's Firmware.")
-        try:
-          user_token = jinput["token"]
-        except:
-          user_token = "*"
-        try:
-          with open(filename, 'a') as csv_file:
-            cdt = datetime.now()
-            outstr  = cdt.strftime('%x %X') + delim
-            outstr += str(spindle_name) + delim
-            outstr += str(spindle_id) + delim
-            outstr += str(angle) + delim
-            outstr += str(temperature) + delim
-            outstr += str(battery) + delim
-            outstr += str(gravity) + delim
-            outstr += user_token + delim
-            outstr += str(interval) + delim
-            outstr += str(rssi) + delim
-            outstr += nline
-            csv_file.writelines(outstr)
-            print(repr(addr) + " CSV data written")
-        except Exception as e:
-          print(repr(addr) + " CSV Error: " + str(e))                        
-    except Exception as e:
-      print(repr(addr) + " Error: " + str(e))
-      clientsock.send(rcv_nak)
-      print(repr(addr) + " NAK sent")
-      break
-    clientsock.close()
-    print(repr(addr) + " closed connection")
-        
+    inpstr += str(data.rstrip().decode("utf-8"))
+    if not data: break    
+
+   
+  clientsock.close()
+  
+  if inpstr[0] == "{":
+    if inpstr.find("}") != -1:
+      jinput = json.loads(inpstr)
+      spindle_name = jinput["name"]
+      spindle_id   = jinput["ID"]
+      angle        = jinput["angle"]
+      temperature  = jinput["temperature"]
+      battery      = jinput["battery"]
+      try:
+        gravity = jinput['gravity']
+        interval = jinput['interval']
+        rssi = jinput['RSSI']
+      except:
+        print("old fw")
+      try:
+        user_token = jinput['token']
+      except:
+        user_token = '*'        
+      try:
+        with open(filepath+str(spindle_name)+".csv", 'a') as csv_file:
+          cdt = datetime.now()
+          outstr  = cdt.strftime("D:%x,C:%X") + delim
+          outstr += "A:"+str(angle)       + delim
+          outstr += "G:"+str(gravity)     + delim
+          outstr += "T:"+str(temperature) + delim
+          outstr += "B:"+str(battery)     + delim
+          outstr += "I:"+str(interval)    + delim
+          outstr += nline
+          csv_file.writelines(outstr)
+          print(outstr)
+          print(repr(addr) + " CSV data written")
+      except IOError:
+        print(repr(addr) + " CSV Error: ")
+    
 def main() :
-  net_addr = (srv_host, srv_port)
+  net_addr = ("0.0.0.0", srv_port)
   serversock = socket(AF_INET, SOCK_STREAM)
   serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
   serversock.bind(net_addr)
@@ -97,5 +83,4 @@ def main() :
     print("connected: " + str(addr))
     _thread.start_new_thread(recv_handler, (clientsock, addr))
 
-if __name__ == "__main__":
-  main()
+main()
